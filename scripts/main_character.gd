8f30fadf1448d615d4061ed1c0a813fc001fa086
Sean Landis <sean.a.lanids@gmail.com>
2025-02-20 12:39:46 -0800

    camea fixes

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -1,3 +1,4 @@
+class_name MainCharacter
 extends RigidBody2D
 
 var exploded := false
@@ -6,16 +7,13 @@ var explode_position := Vector2.ZERO
 func _input(event):
 	if event is InputEventMouseButton and event.is_pressed():
 		exploded = true
-		explode_position = event.position
-		print(event.position)
+		explode_position = get_global_mouse_position()
 
 
 func _physics_process(delta: float) -> void:
 	if exploded:
-		print(explode_position)
-
-		# FIX: Bug - camera and mouse click position vector ?
-		var force_vector := self.position.direction_to(explode_position)
+		var force_vector := explode_position.direction_to(self.position)
 		var force_distance := explode_position.distance_to(self.position)
-		apply_central_impulse(force_vector * (100000/force_distance))
+		
+		apply_central_impulse(force_vector * (10000/force_distance))
 		exploded = false
