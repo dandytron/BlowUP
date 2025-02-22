11dedc370f9db46e72bcb66d7836de4f7f0419c1
Matt Nodurfth <mnodurft@gmail.com>
2025-02-22 10:44:25 -0800

    Add Score!

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+@onready var score_label := %ScoreLabel
+@onready var john := %John
+
+
+func calc_score():
+	var score : int = floor(john.position.y) * -1 + 1023
+	var label_text := "High Score: %s" % [ score ]
+	score_label.text = label_text
+
+
+func _process(_delta):
+	calc_score()
