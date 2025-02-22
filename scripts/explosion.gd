18c2dac788f83b52c6c8d2055e9354423cbf0b45
Matt Nodurfth <mnodurft@gmail.com>
2025-02-22 09:52:47 -0800

    Refactor - remove unneeded parent node; connect signal with new syntax

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -2,7 +2,8 @@ extends AnimatedSprite2D
 
 func _ready():
 	play()
-	connect("animation_finished", Callable(self, "_on_animation_finished"))
+	animation_finished.connect(_on_animation_finished)
+
 
 func _on_animation_finished():
 	queue_free()
