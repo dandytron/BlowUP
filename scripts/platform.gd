5a2ba4b6c3c793a22e9783753fe78b43c9b757cf
Matt Nodurfth <mnodurft@gmail.com>
2025-02-23 21:34:32 -0800

    Fix: Player was't a perfect square; add note about sliding bug

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -7,5 +7,6 @@ func _ready():
 func _on_body_entered(body):
 	# NOTE: This works, but doesn't account for glancing (non stationary) blows to the platform corners
 	# This may not be a problem since the detection area is far enough from the right and left corners
+	# BUG: Sliding on platforms do not reset bomb_count
 	if body.name == "John" && body.global_position.y < self.global_position.y:
 		SignalBus.john_landed.emit()
